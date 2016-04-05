814e6898ab5bcc40693334e4b7d2c8d294e7028d
Andrew Boles <drew.boles88@gmail.com>
2016-04-05 16:15:23 -0500

    Update quantmu.m

--- quantmu.m
+++ quantmu.m
@@ -1,13 +1,12 @@
 function quant = quantmu(x, B, mu)
 % this is mu law quantization
-xmax = max(abs(x));
+%xmax = max(abs(x));
 denom = log(1+mu);
+sign_in = sign(x);
 L = length(x);
 quant = zeros(size(x), 'double');
 for i=1:L
-    quant(i) = xmax*((log(1+(mu*abs(x(i)))/xmax))/denom)*sign(x(i));
+    %quant(i) = xmax(1)*((log(1+(mu*abs(x(i)))/xmax(1)))/denom)*sign(x(i));
+    quant(i) = sign_in(i)*(log(1+(mu*abs(x(i))))/denom);
 end
-
-
-
 end
